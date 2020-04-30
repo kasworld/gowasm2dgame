f2e95d245ab95cfe44ec2084d5e22e921b442e8f
SeukWon Kang <kasworld@gmail.com>
2020-04-30 16:29:12 +0900

    apply genenum change

--- build.sh
+++ build.sh
@@ -45,19 +45,19 @@ goimports -w .
 
 cd ..
 
-genenum -typename=TeamType -packagename=teamtype -basedir=enum -statstype=int
+genenum -typename=TeamType -packagename=teamtype -basedir=enum -vectortype=int
 goimports -w enum/teamtype/teamtype_gen.go
 goimports -w enum/teamtype_stats/teamtype_stats_gen.go
 
-genenum -typename=ActType -packagename=acttype -basedir=enum -statstype=int
+genenum -typename=ActType -packagename=acttype -basedir=enum -vectortype=int
 goimports -w enum/acttype/acttype_gen.go
 goimports -w enum/acttype_stats/acttype_stats_gen.go
 
-genenum -typename=GameObjType -packagename=gameobjtype -basedir=enum -statstype=int
+genenum -typename=GameObjType -packagename=gameobjtype -basedir=enum -vectortype=int
 goimports -w enum/gameobjtype/gameobjtype_gen.go
 goimports -w enum/gameobjtype_stats/gameobjtype_stats_gen.go
 
-genenum -typename=EffectType -packagename=effecttype -basedir=enum -statstype=int
+genenum -typename=EffectType -packagename=effecttype -basedir=enum -vectortype=int
 goimports -w enum/effecttype/effecttype_gen.go
 goimports -w enum/effecttype_stats/effecttype_stats_gen.go
 
